df298d098e1caf47e0609cb9752ba5a44fa99e6c
ssparshsethi <142225493+ssparshsethi@users.noreply.github.com>
2023-10-01 15:36:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL('BirthdayImage.jpg',function(Imh) {
+    block_image-object = Img;
+    block_image_object . scaleTowidth(700);
+    block_image_object. scaleToheight(510)  ;
+    block_image_object . set( {
+top:0,
+left:0 
+        } );
+        canvas .add(block_image_object);
+    });
+}   
+
+function playSound(){
+x.play();
+}
